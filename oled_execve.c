d4f015ad792170c5ff2f2cdf1b6b2c90467addce
ValdikSS <iam@valdikss.org.ru>
2017-07-28 01:31:57 +0300

    Rework execve

--- oled_execve.c
+++ oled_execve.c
@@ -6,14 +6,33 @@
  * arm-linux-androideabi-gcc -fPIC -O2 -s -o oled oled_execve.c
  */
 #include <stdio.h>
+#include <sys/types.h>
 
-void main() {
-    char *const args[] = {"/app/bin/oled.orig", NULL};
-    char *const envs[] = {"LD_PRELOAD=/app/bin/oled_hijack/oled_hijack.so",
-        "LD_LIBRARY_PATH=/vendor/lib:/system/lib:/app/lib",
-        "ANDROID_ROOT=/system",
-        "PATH=/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin:/app/bin",
-        "ANDROID_DATA=/data",
-        NULL};
-    execve("/app/bin/oled.orig", args, envs);
+int main(int argc, char *argv[], char *envp[]) {
+    char *envp_oled[100];
+    char *oled_orig = "/app/bin/oled.orig";
+    char *ldpreload = "LD_PRELOAD=/app/bin/oled_hijack/oled_hijack.so";
+    char *args[] = {oled_orig, NULL};
+
+    int i, status;
+    pid_t pid;
+
+    for (i = 0; envp[i] != NULL; i++) {
+        envp_oled[i] = envp[i];
+    }
+    envp_oled[i] = ldpreload;
+    envp_oled[i+1] = NULL;
+
+    pid = fork();
+    if (pid < 0) {
+        return 1;
+    }
+
+    if (pid > 0) {
+        waitpid(pid, &status, 0);
+        return 1;
+    }
+    else {
+        execve(oled_orig, args, envp_oled);
+    }
 }
